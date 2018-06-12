7d7b51fdc09a7589c71ec49cb4fb986798f7a2e3
PeinearyDevelopment <peinearydevelopment@gmail.com>
2018-06-12 15:38:44 -0400

    added scrollbar styles

--- angular/src/app/dialog.component.ts
+++ angular/src/app/dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as lorem from 'lorem-ipsum';
+
 enum DialogSize {
   Standard = 0,
   Small = 1,
@@ -17,17 +19,35 @@ enum DialogSize {
 
   <dialog [open]="isOpen" (close)="isOpen = false" [ngClass]="getDialogSize()">
     <i class="close" (click)="closeDialog()">x</i>
-    Hello World!
+    <p>{{lorem}}</p>
   </dialog>
-  `
+  `,
+  styles: [
+    `p { height: calc(100% - 16px); }`,
+    `p { overflow-y: auto; }`,
+    `p { margin: 16px 0 0; }`,
+    `p { word-break: break-all; }`,
+    `p { word-break: break-word; }`
+  ]
 })
 export class DialogComponent {
   DialogSize = DialogSize;
 
   private size: DialogSize;
+
   isOpen = false;
+  lorem = '';
 
   openDialog(size: DialogSize) {
+    this.lorem = lorem({
+      count: 7                      // Number of words, sentences, or paragraphs to generate.
+    , units: 'paragraphs'           // Generate words, sentences, or paragraphs.
+    , sentenceLowerBound: 5         // Minimum words per sentence.
+    , sentenceUpperBound: 15        // Maximum words per sentence.
+    , paragraphLowerBound: 3        // Minimum sentences per paragraph.
+    , paragraphUpperBound: 7        // Maximum sentences per paragraph.
+    , format: 'plain'               // Plain text or html
+  });
     this.size = size;
     this.isOpen = true;
   }
